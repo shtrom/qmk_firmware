598b0a92b2d3bff49db3d46d29994a66de380809
Olivier Mehani <omehani@mozilla.com>
2025-06-29 21:18:40 +1000

    add build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -x
+
+FILES="ergodox_ez_base_layout_ergodox_pretty_shtrom.json kradoindustries_promenade_layout_1x2u_shtrom.json"
+
+for f in ${FILES}; do
+	KEYBOARD=$(jp -u @.keyboard < $f)
+	KEYMAP=$(jp -u @.keymap < $f)
+	qmk import-keymap ${f}
+	qmk compile -kb ${KEYBOARD} -km ${KEYMAP}
+done
